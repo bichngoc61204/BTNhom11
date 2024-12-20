7ce893c8cb2fb58016d56c6c3c190479d44fb8d7
nguyenleanhtho <tn040134@gamil.com>
2024-12-20 23:27:40 +0700

    giải quyết xung đột

--- app/src/main/java/com/example/btnhom/MainActivity.java
+++ app/src/main/java/com/example/btnhom/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.btnhom;
 
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
+
 import android.view.View;
-=======
->>>>>>> 275701364a8fe87c751ae6987c7f9969568f45dd
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -25,16 +23,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Tạo Intent để chuyển đến NewActivity
-                Intent intent = new Intent(MainActivity.this, TaskActivity.class);
+                Intent intent = new Intent(MainActivity.this, activity_ngoc.class);
                 startActivity(intent); // Mở NewActivity
             }
         });
 
-        // Xử lý khi nhấn vào nút
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, activity_ngoc.class);
-            startActivity(intent);
-        });
+
     }
 }
