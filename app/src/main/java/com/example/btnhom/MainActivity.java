d50a7f205cfd65a8136e6dafd550ce8b1f540180
ahthog <tn040134@gmail.com>
2024-12-20 22:46:45 +0700

    anh tho

--- app/src/main/java/com/example/btnhom/MainActivity.java
+++ app/src/main/java/com/example/btnhom/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.btnhom;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button button = findViewById(R.id.button_open);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Tạo Intent để chuyển đến NewActivity
+                Intent intent = new Intent(MainActivity.this, TaskActivity.class);
+                startActivity(intent); // Mở NewActivity
+            }
         });
     }
 }
