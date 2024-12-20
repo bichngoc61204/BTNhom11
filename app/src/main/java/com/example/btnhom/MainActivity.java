13a069a62f637aa45399efaa29771cd153064cdd
nguyenleanhtho <tn040134@gamil.com>
2024-12-21 00:27:57 +0700

    Reset

--- app/src/main/java/com/example/btnhom/MainActivity.java
+++ app/src/main/java/com/example/btnhom/MainActivity.java
@@ -18,15 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button_open);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        //Button button = findViewById(R.id.button_open);
+       // button.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View view) {
                 // Tạo Intent để chuyển đến NewActivity
                 //Intent intent = new Intent(MainActivity.this, activity_ngoc.class);
                 //startActivity(intent); // Mở NewActivity
-            }
-        });
+            //}
+        //});
 
 
     }
